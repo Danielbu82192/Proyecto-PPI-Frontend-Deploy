be7668ba16337ce403cdf3aef6f764b3f4cf0524
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-21 15:23:49 -0500

    Campo de limpiar sistema

--- src/app/component/coordinador/limpiarSistema/page.jsx
+++ src/app/component/coordinador/limpiarSistema/page.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
 import React, { useState } from 'react'
+import { useRouter } from "next/navigation";
 
 function page() {
 
     const [showAlertDelete, setShowAlertDelete] = useState(false);
+    const [showCorrecto, setShowCorrecto] = useState(false);
+    const router = useRouter();
 
     const Limpiar = async () => {
         const requestOptions = {
@@ -12,10 +15,23 @@ function page() {
             headers: { 'Content-Type': 'application/json' },
         };
         const response = await fetch('https://td-g-production.up.railway.app/usuario/limpiarSistema/', requestOptions);
-        if(response.ok)
-            //alert()
+        if(response.ok){
+            setShowCorrecto(true)
+            setTimeout(() => {
+                router.push('/');
+            }, 2000);
+        } 
     }
 
+    useEffect(() => {
+        if (showCorrecto) {
+            const timer = setTimeout(() => {
+                setShowCorrecto(false);
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [showCorrecto]);
     return (
         <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
             <div className='pt-8  pb-8 w-full'>
@@ -62,6 +78,25 @@ function page() {
                 </div>
             </>
             )}
+            {
+                showCorrecto && (
+                    <div className="fixed bottom-0 right-0 mb-8 mr-8">
+                        <div className="flex w-96 shadow-lg rounded-lg">
+                            <div class="bg-green-600 py-4 px-6 rounded-l-lg flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="text-white fill-current" viewBox="0 0 16 16" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
+                            </div>
+                            <div className="px-4 py-6 bg-white rounded-r-lg flex justify-between items-center w-full border border-l-transparent border-gray-200">
+                                <div>El sistema se limpió correctamente.</div>
+                                <button onClick={() => { setShowCorrecto(!showCorrecto) }}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current text-gray-700" viewBox="0 0 16 16" width="20" height="20">
+                                        <path fillRule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </div >
     )
 }
