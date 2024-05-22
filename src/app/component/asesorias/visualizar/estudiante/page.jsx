f5ce52028a6dded4b7a69c2624ec55c946dbcd5e
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-22 00:48:34 -0500

    Modificación del titulo para ser responsivo

--- src/app/component/asesorias/visualizar/estudiante/page.jsx
+++ src/app/component/asesorias/visualizar/estudiante/page.jsx
@@ -4,10 +4,10 @@ import Tarjetas from '@/component/asesorias/tarjetas/tarjetas'
 function page() {
     return (
         <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
-            <div className='pt-8  pb-8 w-full'>
-                <div className=' md:h-22 lg:h-22 xl:h-16 sm:h-22   border-b-2 sm:pl-8 flex '>
-                    <h1 className='text-4xl font-bold text-center text-gray-600'>Mis citas de asesorías</h1>
-                </div>
+            <div className='pt-8  pb-8 w-full'> 
+                <div className='w-full border-b-2 flex items-center sm:items-start justify-center sm:justify-start sm:pl-8 sm:h-22 pb-5 text-center sm:text-left'>
+                        <h1 className='text-4xl font-bold text-gray-600'>Mis citas de asesorías</h1>
+                    </div>
                 <div className='p-10'>
                     <Tarjetas />
                 </div>
