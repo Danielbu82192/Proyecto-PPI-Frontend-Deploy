c69eabbf9db68c796c8dd78736eb79ef21c41ccb
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-22 01:59:29 -0500

    Actualización Responsivo

--- src/app/component/bitacora/visualizar/modSol/[id]/page.jsx
+++ src/app/component/bitacora/visualizar/modSol/[id]/page.jsx
@@ -8,10 +8,8 @@ function page({ params }) {
     return (
         <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
             <div className='pt-8  pb-8 w-full'>
-                <div className='md:h-22 lg:h-22 xl:h-22 sm:h-22 border-b-2 pl-8 pb-5 pr-52 flex justify-between items-center'>
-                    <div>
-                        <h1 className='text-4xl font-bold text-gray-600'>Bitácoras del equipo {params.id}</h1>
-                    </div>
+                <div className='w-full border-b-2 flex items-center sm:items-start justify-center sm:justify-start sm:pl-8 sm:h-22 pb-5 text-center sm:text-left'>
+                    <h1 className='text-4xl font-bold text-gray-600'>Bitácoras del equipo {params.id}</h1>
                 </div>
                 <div className='p-10'>
                     <CrearBitacora equipo={params.id} />
