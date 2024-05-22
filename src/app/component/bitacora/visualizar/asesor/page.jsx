62c91e753ab3176fd03c78dc7a1686cac64471dc
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-22 02:21:05 -0500

    Actualización Responsivo

--- src/app/component/bitacora/visualizar/asesor/page.jsx
+++ src/app/component/bitacora/visualizar/asesor/page.jsx
@@ -3,18 +3,17 @@ import Asesor from '@/component/bitacora/visualizar/asesor/asesor'
 
 function page() {
     return (
-    <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
-        <div className='pt-8  pb-8 w-full'>
-            <div className='md:h-22 lg:h-22 xl:h-22 sm:h-22 border-b-2 pl-8 pb-5 pr-52 flex justify-between items-center'>
-                <div>
+        <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
+            <div className='pt-8  pb-8 w-full'>
+
+                <div className='w-full border-b-2 flex items-center sm:items-start justify-center sm:justify-start sm:pl-8 sm:h-22 pb-5 text-center sm:text-left'>
                     <h1 className='text-4xl font-bold text-gray-600'>Bitácoras</h1>
                 </div>
-            </div>
-            <div className='p-10'>
-                <Asesor />
+                <div className='p-10'>
+                    <Asesor />
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
