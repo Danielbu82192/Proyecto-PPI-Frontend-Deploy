ecf3ddd0bd173fef845277b810f737c8f5119309
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-22 00:25:52 -0500

    Modificación del titulo para ser responsivo

--- src/app/component/asesorias/visualizar/estudiante/page.jsx
+++ src/app/component/asesorias/visualizar/estudiante/page.jsx
@@ -5,7 +5,7 @@ function page() {
     return (
         <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
             <div className='pt-8  pb-8 w-full'>
-                <div className=' md:h-22 lg:h-22 xl:h-16 sm:h-22  border-b-2 pl-8 pr-80 items-start w-full flex '>
+                <div className=' md:h-22 lg:h-22 xl:h-16 sm:h-22   border-b-2 sm:pl-8 flex '>
                     <h1 className='text-4xl font-bold text-center text-gray-600'>Mis citas de asesorías</h1>
                 </div>
                 <div className='p-10'>
