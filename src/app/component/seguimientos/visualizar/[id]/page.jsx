4547bd3af44af5d53a266ec11266af36ff435c2c
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-22 01:40:24 -0500

    Actualización Responsivo

--- src/app/component/seguimientos/visualizar/[id]/page.jsx
+++ src/app/component/seguimientos/visualizar/[id]/page.jsx
@@ -19,13 +19,13 @@ function page({ params }) {
     traerEstado();
   }, [params]);
 
-  
+
   useEffect(() => {
     const traerEstado = async () => {
-      if (!seguimiento) return; 
-      setEstudiantes([]); 
+      if (!seguimiento) return;
+      setEstudiantes([]);
       const promises = [];
-  
+
       if (seguimiento.estudiante1 != null) {
         promises.push(fetchUsuario(seguimiento.estudiante1, seguimiento.asistenciaEstudiante1));
       }
@@ -34,14 +34,14 @@ function page({ params }) {
       }
       if (seguimiento.estudiante3 != null) {
         promises.push(fetchUsuario(seguimiento.estudiante3, seguimiento.asistenciaEstudiante3));
-      } 
-      const estudiantesData = await Promise.all(promises); 
+      }
+      const estudiantesData = await Promise.all(promises);
       setEstudiantes(estudiantesData);
     };
-  
+
     traerEstado();
   }, [seguimiento]);
-   
+
   const fetchUsuario = async (usuarioId, asistencia) => {
     const response = await fetch(`https://td-g-production.up.railway.app/usuario/${usuarioId}`);
     const usuarioData = await response.json();
@@ -51,46 +51,42 @@ function page({ params }) {
   return (
     <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
       <div className='pt-8  pb-8 w-full'>
-        <div className='md:h-22 lg:h-22 xl:h-22 sm:h-22 border-b-2 pl-8 pb-5 pr-52 flex justify-between items-center'>
-          <div>
-            <h1 className='text-4xl font-bold text-gray-600'>Visualizar asesoría</h1>
-          </div>
+        <div className='w-full border-b-2 flex items-center sm:items-start justify-center sm:justify-start sm:pl-8 sm:h-22 pb-5 text-center sm:text-left'>
+          <h1 className='text-4xl font-bold text-gray-600'>Visualizar asesoría</h1>
         </div>
-        <div className='p-10 '>
-          <div className='text-center grid grid-cols-2'>
+        <div className='p-10'>
+          <div className='text-center grid grid-cols-1 sm:grid-cols-2 gap-4'>
             <div>
               <h1 className='text-2xl text-gray-600 font-bold'>Compromisos:</h1>
-              <div className='text-xl text-gray-500 font-semibold'>{seguimiento.compromiso}</div>
+              <div className='text-xl text-gray-500 font-semibold break-words'>{seguimiento.compromiso}</div>
             </div>
             <div>
               <h1 className='text-2xl text-gray-600 font-bold'>Observaciones:</h1>
-              <div className='text-xl text-gray-500 font-semibold'>{seguimiento.observacion}</div>
+              <div className='text-xl text-gray-500 font-semibold break-words'>{seguimiento.observacion}</div>
             </div>
           </div>
           <div>
-
             <div className='pt-5 text-center'>
               <h1 className='text-2xl text-gray-600 font-bold'>Asistencias:</h1>
               <div>
                 {estudiantes.map((item) => (
-                  <div className='grid grid-cols-2 mt-3' key={item[0].id}>
+                  <div className='grid grid-cols-1 sm:grid-cols-2 mt-3' key={item[0].id}>
                     <div>
                       <span className='text-xl text-gray-500 font-semibold'>
                         {item[0].nombre}
                       </span>
                     </div>
-                    <div>
-                      <span className={item[1] == 0 ||item[1] ==null ? ('text-xl px-5 py-1 rounded-xl bg-red-500 text-white font-semibold max-w-[5px]') : ('text-xl px-8 py-1 rounded-xl bg-emerald-500 text-white font-semibold')}>
-                        {item[1] == 0||item[1] ==null ? (<>No asistió.</>) : (<>Asistió.</>)}
+                    <div className='mt-2 sm:mt-0'>
+                      <span className={item[1] == 0 || item[1] == null ? 'text-xl px-5 py-1 rounded-xl bg-red-500 text-white font-semibold' : 'text-xl px-8 py-1 rounded-xl bg-emerald-500 text-white font-semibold'}>
+                        {item[1] == 0 || item[1] == null ? 'No asistió.' : 'Asistió.'}
                       </span>
                     </div>
                   </div>
                 ))}
-
               </div>
             </div>
           </div>
-        </div>
+        </div> 
       </div>
     </div>
   )
