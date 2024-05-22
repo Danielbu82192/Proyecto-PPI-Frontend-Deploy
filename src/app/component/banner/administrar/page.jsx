edf275ec574e8d3fa4d42683935ffae80e1b9809
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-22 12:32:40 -0500

    Actualización Responsivo

--- src/app/component/banner/administrar/page.jsx
+++ src/app/component/banner/administrar/page.jsx
@@ -4,9 +4,9 @@ import BannerTable from '@/component/banner/bannerTable'
 function Page() {
     return (
         <div className="ml-6 mr-6 mt-6 border bg-white border-b flex justify-between">
-            <div className='pt-8 pb-2 w-full'>
-                <div className=' md:h-22 lg:h-16 xl:h-22 sm:h-22  border-b-2 pl-8 items-start'>
-                    <h2 className='text-4xl font-bold text-left text-gray-600'>Administrar: Contenidos informativos</h2>
+            <div className='pt-8 pb-2 w-full'> 
+                <div className='w-full border-b-2 flex flex-col sm:flex-row items-center sm:items-start justify-between sm:pl-8 sm:h-22 sm:pr-5 pb-5 text-center sm:text-left'>
+                    <h1 className='text-4xl font-bold text-gray-600 mb-2 sm:mb-0'>Administrar: Contenidos informativos</h1>
                 </div>
                 <div className='m-2'>
                     <BannerTable />
